de5f555b52c08e674b148b88e0ad755761638484
Danielle Puga <danielle.puga@gmail.com>
2021-05-09 15:59:23 -0700

    button class

--- main.rb
+++ main.rb
@@ -1,10 +1,17 @@
 def draw_button(label_text, x_position, y_position, foreground_hex_code, in_dark_mode)
   button = Button.new(label_text, x_position, y_position, foreground_hex_code, in_dark_mode)
   _paint(button)
+end
 
 def _paint(self)
-  paint(self.label_text, self.x_position, self.y_position, foreground_hex_code, button.foregroundcolor
+  paint(self.label_text, self.x_position, self.y_position, foreground_hex_code, button.color)
+end
 
+class Button 
+  attr_reader :name, :x_position, :y_position, :foreground_hex_code, :color
+
+  def initialize(name, x_position, y_position, foreground_hex_code, dark)
+    @label
 def change_color(self, foreground_hex_code, in_dark_mode)
 
   if in_dark_mode
