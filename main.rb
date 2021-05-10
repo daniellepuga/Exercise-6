f809680848c28556168f5e33ca8dc447b6bdf1be
Danielle Puga <danielle.puga@gmail.com>
2021-05-09 17:13:57 -0700

    finished refactor and removed comments

--- main.rb
+++ main.rb
@@ -7,20 +7,26 @@ def _paint(self)
   paint(self.label_text, self.x_position, self.y_position, foreground_hex_code, button.color)
 end
 
-class Button 
+class Button
+
   attr_reader :name, :x_position, :y_position, :foreground_hex_code, :color
 
   def initialize(name, x_position, y_position, foreground_hex_code, dark)
-    @label
+    @name = name
+    @x_position = x_position
+    @y_position = y_position
+    @foreground_hex_code = foreground_hex_code
+    @dark = dark
+    @hex_code = ""
+    change_color()
+
 def change_color(self, foreground_hex_code, in_dark_mode)
-
-  if in_dark_mode
-    # darken foreground color for dark mode
-    paint(label_text, x_position, y_position, foreground_hex_code - 10, '#111111')
-
-  else
-    # lighten foreground color for non-dark mode
-    paint(label_text, x_position, y_position, foreground_hex_code + 10, '#E0E0E0')
-  end
+    if @dark == True:
+      @foreground_hex_code = @foreground_hex_code - 10
+      @hex_code = "#E0E0E0"
+    else
+      @foreground_hex_code = @foreground_hex_code + 10
+      @hex_code = "111111"
+    end
 
 end
