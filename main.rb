bab526d1c535a54b21976bb1a8d0fd16d2ce3e53
Danielle Puga <danielle.puga@gmail.com>
2021-05-09 17:23:41 -0700

    fixed logic and variable names. removed unnecessary function.

--- main.rb
+++ main.rb
@@ -1,10 +1,5 @@
 def draw_button(label_text, x_position, y_position, foreground_hex_code, in_dark_mode)
   button = Button.new(label_text, x_position, y_position, foreground_hex_code, in_dark_mode)
-  _paint(button)
-end
-
-def _paint(self)
-  paint(self.label_text, self.x_position, self.y_position, foreground_hex_code, button.color)
 end
 
 class Button
@@ -19,14 +14,16 @@ class Button
     @dark = dark
     @hex_code = ""
     change_color()
+  end
 
-def change_color(self, foreground_hex_code, in_dark_mode)
+def change_color(self, foreground_hex_code, dark)
     if @dark == True:
-      @foreground_hex_code = @foreground_hex_code - 10
+      @foreground_hex_code = @foreground_hex_code + 10
       @hex_code = "#E0E0E0"
     else
-      @foreground_hex_code = @foreground_hex_code + 10
+      @foreground_hex_code = @foreground_hex_code - 10
       @hex_code = "111111"
     end
+  end
 
 end
